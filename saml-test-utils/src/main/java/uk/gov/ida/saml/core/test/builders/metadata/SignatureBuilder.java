d63ccc199f6ae46f559702a1446c1e536267d7df
Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>
2019-01-30 11:06:08 +0000

    bau: extract saml-test-utils
    We've been shipping code we use for testing our production code within
    our core libraries (Junit, Mockito, AssertJ, our fake PKI codebase etc).
    We shouldn't be including code that we haven't tested or intend to use
    in production when we ship code so I've extracted out this test code
    into a separate help library that can be used as a test dependency  when
     we test our applications.
    
    In some cases this is causing issues on the classpath and when we
    develop our code as without some very explicit configuration in gradle
    we can't be sure what version of the dependencies are included when we
    compile or run tests.

--- saml-test-utils/src/main/java/uk/gov/ida/saml/core/test/builders/metadata/SignatureBuilder.java
+++ saml-test-utils/src/main/java/uk/gov/ida/saml/core/test/builders/metadata/SignatureBuilder.java
@@ -13,7 +13,7 @@ import org.opensaml.xmlsec.signature.X509Certificate;
 import org.opensaml.xmlsec.signature.support.DocumentInternalIDContentReference;
 import org.opensaml.xmlsec.signature.support.SignatureConstants;
 import uk.gov.ida.saml.core.test.TestCertificateStrings;
-import uk.gov.ida.saml.metadata.test.factories.metadata.TestCredentialFactory;
+import uk.gov.ida.saml.core.test.TestCredentialFactory;
 
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
